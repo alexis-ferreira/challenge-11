960a85ba3b19d5e19fca36e8d3505e3f54b46494
Alexis Ferreira <alexisferreira@MacBook-Pro-de-Alexis.local>
2021-08-16 12:32:18 +0200

    Challenge completed

--- js/script.js
+++ js/script.js
@@ -61,3 +61,81 @@ btnBurgerMenu.addEventListener('click', function () {
         }, 500)
     }
 })
+
+
+
+
+// HOVER
+
+let btnRequest = document.getElementsByClassName('btn-request');
+console.log(btnRequest);
+
+for (let i = 0; i < btnRequest.length; i++) {
+
+    btnRequest[i].addEventListener('mouseenter', function () {
+
+        btnRequest[i].style.transition = "background 300ms";
+        btnRequest[i].style.background = "linear-gradient(90deg, rgba(49, 211, 92, 0.53) 49%, rgba(43, 218, 183, 0.59) 100%)";
+    })
+
+    btnRequest[i].addEventListener('mouseleave', function () {
+
+        btnRequest[i].style.transition = "background 300ms";
+        btnRequest[i].style.background = "linear-gradient(90deg, rgba(49, 211, 92, 1) 49%, rgba(43, 218, 183, 1) 100%)";
+    })
+}
+
+let menuDesktop = document.getElementsByClassName('link-menu');
+console.log(menuDesktop);
+
+let borderBottomMenu = document.getElementsByClassName('bottom-border-menu');
+console.log(borderBottomMenu);
+
+for (let i = 0; i < menuDesktop.length; i++) {
+
+    menuDesktop[i].addEventListener('mouseenter', function () {
+
+        borderBottomMenu[i].style.transition = "width 300ms";
+        borderBottomMenu[i].style.width = "100%";
+    })
+
+    menuDesktop[i].addEventListener('mouseleave', function () {
+
+        borderBottomMenu[i].style.transition = "width 300ms";
+        borderBottomMenu[i].style.width = "0";
+    })
+}
+
+let articleTitle = document.getElementsByClassName('article-title');
+console.log(articleTitle);
+
+for (let i = 0; i < articleTitle.length; i++) {
+
+    articleTitle[i].addEventListener('mouseenter', function () {
+
+        articleTitle[i].style.transition = "color 300ms";
+        articleTitle[i].style.color = "rgb(112, 210, 187)";
+    })
+
+    articleTitle[i].addEventListener('mouseleave', function () {
+
+        articleTitle[i].style.color = "black";
+    })
+}
+
+let menuFooter = document.getElementsByClassName('link-menu-footer');
+console.log(menuFooter);
+
+for (let i = 0; i < menuFooter.length; i++) {
+
+    menuFooter[i].addEventListener('mouseenter', function () {
+
+        menuFooter[i].style.transition = "color 300ms";
+        menuFooter[i].style.color = "rgb(112, 210, 187)";
+    })
+
+    menuFooter[i].addEventListener('mouseleave', function () {
+
+        menuFooter[i].style.color = "white";
+    })
+}
